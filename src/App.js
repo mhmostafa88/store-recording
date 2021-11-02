3f9f38817e61b2e020bf9fb4126df6cfcab66dfc
codingwizard88 <mahmoud@mightydigits.com>
2021-11-02 09:41:32 +0200

    init commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
 import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import React, { Component } from 'react';
+import { Switch, Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import ProductList from './components/ProductList';
+import Details from './components/Details';
+import Cart from './components/Cart';
+import Default from './components/Default';
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+    <Navbar />
+    {/* Switch will start after Navbar because Navbar is present on all pages */}
+    <Switch>
+      <Route exact path="/" component={ProductList} />
+      <Route path="/details" component={Details} />
+      <Route path="/cart" component={Cart} />
+      <Route component={Default} />
+    </Switch>
+    <ProductList />
+    <Details />
+    <Cart />
+    <Default />
+    </React.Fragment>
   );
 }
 
